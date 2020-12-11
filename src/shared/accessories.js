14906a7be6aebbb333ffcab214e424879983ca68
AJ <ancheetah@users.noreply.github.com>
2020-12-11 17:06:25 -0500

    created react app; added images; added product info

---
+++ src/shared/accessories.js
@@ -0,0 +1,113 @@
+export const ACCESSORIES = [
+    {
+        id: 0,
+        name: "Supreme/Colgate Toothpaste",
+        image: "/assets/img/Supreme_Colgate_Toothpaste-White_1024x1024.jpg",
+        availability: "In Stock",
+        description: "Description",
+        color: ["white"],
+        sku: 74920,
+        rating: 0,
+        price: 50
+    },
+    {
+        id: 1,
+        name: "Supreme Metal Folding Chair",
+        image: "/assets/img/SupremeMetalFoldingChair-Black_1024x1024.jpg",
+        availability: "In Stock",
+        description: "Description",
+        color: ["black"],
+        sku: 93723,
+        rating: 1,
+        price: 250
+    },
+    {
+        id: 2,
+        name: "Supreme Steiff Bear",
+        image: "/assets/img/bears_1024x1024.jpg",
+        availability: "Out of Stock",
+        description: "Description",
+        color: ["heather gray"],
+        sku: 15406,
+        rating: 2,
+        price: 430
+    },
+    {
+        id: 3,
+        name: "Supreme / Sasquatchfabrix Folding Fan",
+        image: "/assets/img/SupremeSasquatchfabrixFoldingFan-White_1024x1024.jpg",
+        availability: "In Stock",
+        description: "Description",
+        color: ["white"],
+        sku: 39072,
+        rating: 3,
+        price: 300
+    },
+    {
+        id: 4,
+        name: "Supreme Bolt Cutters",
+        image: "/assets/img/bolt_1024x1024.jpg",
+        availability: "Out of Stock",
+        description: "Description",
+        color: ["red"],
+        sku: 12345,
+        rating: 4,
+        price: 350
+    },
+    {
+        id: 5,
+        name: "Supreme Crowbar",
+        image: "/assets/img/corw_1024x1024.jpg",
+        availability: "Out of Stock",
+        description: "Description",
+        color: ["red"],
+        sku: 54321,
+        rating: 5,
+        price: 345
+    },
+    {
+        id: 6,
+        name: "Supreme Diamond Cut Zippo",
+        image: "/assets/img/ILVER_1024x1024.jpg",
+        availability: "Out of Stock",
+        description: "Description",
+        color: ["silver"],
+        sku: 36180,
+        rating: 6,
+        price: 300
+    },
+    {
+        id: 7,
+        name: "Supreme Utility Pouch SS20",
+        image: ["/assets/img/SupremeUtilityPouchSS20-Black_1024x1024.jpg",
+                "/assets/img/SupremeUtilityPouchSS20-BlueDesertCamo_1024x1024.jpg"],
+        availability: ["In Stock", "In Stock"],
+        description: "Description",
+        color: ["black","blue desert camo"],
+        sku: [98765, 98764],
+        rating: 7,
+        price: [120, 100]
+    },
+    // {
+    //     id: 8,
+    //     name: "Supreme Utility Pouch SS20",
+    //     image: "",
+    //     availability: "In Stock",
+    //     description: "Description",
+    //     color: ["blue desert camo"],
+    //     sku: 98764,
+    //     rating: 8,
+    //     price: 100
+    // },
+    {
+        id: 9,
+        name: "Supreme Canvas Tote",
+        image: "/assets/img/SupremeCanvasTote-White2_1024x1024.jpg",
+        availability: "In Stock",
+        description: "Description",
+        color: ["white"],
+        sku: 69032,
+        rating: 9,
+        price: 250
+    }
+];
---
+++ src/shared/apparel.js
@@ -0,0 +1,13 @@
+export const APPAREL = [
+    {
+        id: 0,
+        name: "Supreme 20th Anniversary Box Logo Tee",
+        image: "/assets/img/logoo_1024x1024.jpg",
+        availability: "Out of Stock",
+        description: "Description",
+        color: ["white"],
+        sku: 12743,
+        rating: 10,
+        price: 3000
+    }
+];
