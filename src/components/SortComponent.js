462215eec925ee7f6a87923884ae32b822118a75
AJ <ancheetah@users.noreply.github.com>
2021-02-13 13:21:31 -0500

    all sorting options functional

--- src/components/SortComponent.js
+++ src/components/SortComponent.js
@@ -1,30 +1,18 @@
-/*
-1. Create a sort button that when clicked displays a dropdown menu
-    Sort By:
-        Price: Ballin' -> Budget Friendly
-        Price: Budget Friendly -> Ballin'
-        Product: Most Outgrageous -> Absolutely Essential
-        Product: Absolutely Essential -> Most Outgrageous
-
-2. Sort the accessories array
-3. Pass the sorted array as a prop into the Store component when it is called in Main
-*/
-
 import React, { Component } from 'react';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-//import { ACCESSORIES } from '../shared/accessories';
 
 class Sort extends Component {
 
   constructor(props) {
     super(props);
-
     this.state = {
-      isDropdownOpen: false,
-      //accessories: ACCESSORIES
+      isDropdownOpen: false
     };
     this.toggleDropdown = this.toggleDropdown.bind(this);
     this.priceLowToHigh = this.priceLowToHigh.bind(this);
+    this.priceHighToLow = this.priceHighToLow.bind(this);
+    this.ratingLowToHigh = this.ratingLowToHigh.bind(this);
+    this.ratingHighToLow = this.ratingHighToLow.bind(this);
   }
 
   toggleDropdown() {
@@ -35,7 +23,23 @@ class Sort extends Component {
 
   priceLowToHigh() {
     const sortedArr = this.props.products.sort((a,b) => (a.price > b.price) ? 1 : -1);
-    //this.setState({accessories: sortedArr});
+    // console.log("sortedArr: ", sortedArr);
+    this.props.updateProducts(sortedArr);
+  }
+
+  priceHighToLow() {
+    const sortedArr = this.props.products.sort((a,b) => (a.price < b.price) ? 1 : -1);
+    this.props.updateProducts(sortedArr);
+  }
+
+  ratingLowToHigh() {
+    const sortedArr = this.props.products.sort((a,b) => (a.rating > b.rating) ? 1 : -1);
+    console.log("sortedArr: ", sortedArr);
+    this.props.updateProducts(sortedArr);
+  }
+
+  ratingHighToLow() {
+    const sortedArr = this.props.products.sort((a,b) => (a.rating < b.rating) ? 1 : -1);
     console.log("sortedArr: ", sortedArr);
     this.props.updateProducts(sortedArr);
   }
@@ -49,11 +53,11 @@ class Sort extends Component {
         <DropdownMenu>
           <DropdownItem header>Price</DropdownItem>
           <DropdownItem onClick={this.priceLowToHigh}>Budget Friendly to Ballin'</DropdownItem>
-          <DropdownItem onClick="">Ballin' to Budget Friendly</DropdownItem>
+          <DropdownItem onClick={this.priceHighToLow}>Ballin' to Budget Friendly</DropdownItem>
           <DropdownItem divider />
-          <DropdownItem header>Product</DropdownItem>
-          <DropdownItem onClick="">Most Outrageous to Essentials</DropdownItem>
-          <DropdownItem onClick="">Essentials to Most Outrageous</DropdownItem>
+          <DropdownItem header>Rating</DropdownItem>
+          <DropdownItem onClick={this.ratingLowToHigh}>Most Outrageous to Essentials</DropdownItem>
+          <DropdownItem onClick={this.ratingHighToLow}>Essentials to Most Outrageous</DropdownItem>
         </DropdownMenu>
       </Dropdown>
     );
