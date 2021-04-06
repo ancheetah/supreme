f9f338590f1bd2ff93e0dd78635c5561f90f3e0a
AJ <ancheetah@users.noreply.github.com>
2021-04-06 13:46:07 -0400

    test

---
+++ src/components/MainComponent.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Store from './StoreComponent';
+import ProductInfo from './ProductInfoComponent';
+
+const mapStateToProps = state => {
+    return {
+        accessories: state.accessories,
+        apparel: state.apparel
+    };
+};
+
+class Main extends Component {
+    render () {
+        const ProductWithSKU = ({match}) => {
+            return (
+                <ProductInfo 
+                    product={this.props.accessories.accessories.filter(product => 
+                        product.sku === +match.params.productSku)[0]}
+                />
+            );
+        };
+
+        return (
+                <Switch>
+                    <Route exact path='/store' render={() => 
+                        <Store products={this.props.accessories.accessories}/>} /> 
+                    <Route path='/store/:productSku' component={ProductWithSKU} />
+                    <Redirect to='/store' />
+                </Switch>
+        );
+    }
+
+}
+
+export default withRouter(connect(mapStateToProps)(Main));
